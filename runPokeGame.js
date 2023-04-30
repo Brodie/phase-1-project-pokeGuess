019db66040086ea2d8dcc63041775995eb0c0045
Brodie <126362542+Brodie@users.noreply.github.com>
2023-04-30 09:51:08 -0700

    Random Array generation + barebones appending img to dom

--- runPokeGame.js
+++ runPokeGame.js
@@ -1,10 +1,16 @@
 console.log("hi");
 
-// testing fetch with test array.
+// testing random array generator
 // assigning values to object with id, name, pic src keys
 // pushing object to arrayOfPokes
+// barebones appending of poke image to dom
 // ------------------------------------------------------
-let pokeNums = [4, 8, 12, 130];
+
+let pokeNums = []
+for (let i = 0; i < 5; i++) {
+    pokeNums.push(Math.floor(Math.random() * (151 - 1 + 1) + 1))
+}
+
 let arrayOfPokes = [];
 
 pokeNums.forEach((num) => {
@@ -17,5 +23,10 @@ pokeNums.forEach((num) => {
       pokeObj.img = data.sprites.front_default;
       arrayOfPokes.push(pokeObj);
       console.log(arrayOfPokes);
+      let card = document.createElement("div")
+      let pokeImg = document.createElement("img")
+      pokeImg.src = pokeObj.img
+      card.append(pokeImg)
+      document.querySelector("body").append(card)
     });
 });
