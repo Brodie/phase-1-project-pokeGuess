0b0a946e28615f59f2cbf638c2460002d2027ce6
Brodie <126362542+Brodie@users.noreply.github.com>
2023-04-30 12:54:33 -0700

    Testing method to generate array of random non-repeating numbers

--- runPokeGame.js
+++ runPokeGame.js
@@ -1,14 +1,28 @@
 console.log("hi");
 
 // testing random array generator
+// trying to test array against values already generated to avoid duplicate fetches
 // assigning values to object with id, name, pic src keys
 // pushing object to arrayOfPokes
 // barebones appending of poke image to dom
 // ------------------------------------------------------
 
 let pokeNums = []
+let usedNums = []
+function rdmNum (){
+    let rdm = Math.floor(Math.random() * (151 - 1 + 1) + 1)
+    console.log(rdm)
+    return rdm
+}
 for (let i = 0; i < 5; i++) {
-    pokeNums.push(Math.floor(Math.random() * (151 - 1 + 1) + 1))
+    let newNum = rdmNum()
+    if(usedNums.includes(newNum)) {
+        // regenerate new num and try again
+        // recursive calling of this for loop?
+    }else{
+        pokeNums.push(newNum)
+        usedNums.push(newNum)
+    }
 }
 
 let arrayOfPokes = [];
