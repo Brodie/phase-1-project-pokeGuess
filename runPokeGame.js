d26f6fd250f7bc4525639d01f129e3d3f62a0505
Brodie Ashcraft <brodiea19@gmail.com>
2023-05-04 08:52:28 -0700

    refactored code. Fetch happens on load, DOM updated on start button. Avoids issue with forEach and fetch

--- runPokeGame.js
+++ runPokeGame.js
@@ -1,67 +1,68 @@
-/* ToDo next time: 
-add name of pokemon to card element, but hidden
-maybe toy with styles a little to get a feel
-add drop down for types to form, try to style so type drop down is in line with text
-if drop down and text input looks good, start hooking up form! 
-*/
-
 console.log("hi");
 
 // assigning values to object with id, name, pic src keys
 // pushing object to arrayOfPokes
 // barebones appending of poke image to dom
 // ------------------------------------------------------
+
 let pokeNums = [];
 let usedNums = [];
 let arrayOfPokes = [];
+let pokeObj = {};
+
+//need to fetch pokemon before click, then use the fetched object after being clicked to generate
+// the cards. instead of clicking and loading them all
+
+function rdmNum() {
+  let rdm = Math.floor(Math.random() * (151 - 1 + 1) + 1);
+  console.log(rdm);
+  return rdm;
+}
+function arrTester() {
+  let newNum = rdmNum();
+  if (usedNums.includes(newNum)) {
+    arrTester();
+  } else {
+    pokeNums.push(newNum);
+    usedNums.push(newNum);
+  }
+}
+for (let i = 0; i < 5; i++) {
+  arrTester();
+}
+// fetches array of pokeObj on load, preps the array to be iterated through to add card elements
+pokeNums.forEach((num) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
+    .then((res) => res.json())
+    .then((pokemon) => {
+      // create pokeObj and give keys
+      let pokeObj = {};
+      pokeObj.id = num;
+      pokeObj.name = pokemon.name;
+      pokeObj.img = pokemon.sprites.front_default;
+
+      if (pokemon.types.length === 2) {
+        pokeObj.typeOne = pokemon.types[0].type.name;
+        pokeObj.typeTwo = pokemon.types[1].type.name;
+      } else {
+        pokeObj.typeOne = pokemon.types[0].type.name;
+      }
+
+      arrayOfPokes.push(pokeObj);
+      console.log(arrayOfPokes);
+    });
+});
 
 document.getElementById("btn_id").addEventListener("click", () => {
-  function rdmNum() {
-    let rdm = Math.floor(Math.random() * (151 - 1 + 1) + 1);
-    console.log(rdm);
-    return rdm;
-  }
-  function arrTester() {
-    let newNum = rdmNum();
-    if (usedNums.includes(newNum)) {
-      arrTester();
-    } else {
-      pokeNums.push(newNum);
-      usedNums.push(newNum);
-    }
-  }
-  for (let i = 0; i < 5; i++) {
-    arrTester();
-  }
+  arrayOfPokes.forEach((pokeObj, index) => {
+    let card = document.getElementById(`poke_${index + 1}`);
+    let cardTitle = document.createElement("h2");
+    cardTitle.textContent = `Pokemon Number ${index + 1}`;
+    card.append(cardTitle);
 
-  pokeNums.forEach((num, index) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
-      .then((res) => res.json())
-      .then((pokemon) => {
-        let pokeObj = {};
-        pokeObj.id = num;
-        pokeObj.name = pokemon.name;
-        pokeObj.img = pokemon.sprites.front_default;
-        if (pokemon.types.length === 2) {
-          pokeObj.typeOne = pokemon.types[0].type.name;
-          pokeObj.typeTwo = pokemon.types[1].type.name;
-        } else {
-          pokeObj.typeOne = pokemon.types[0].type.name;
-        }
-
-        arrayOfPokes.push(pokeObj);
-        console.log(arrayOfPokes);
-
-        let card = document.getElementById(`poke_${index + 1}`);
-        let cardTitle = document.createElement("h2");
-        cardTitle.textContent = `Pokemon Number ${index + 1}`;
-        card.append(cardTitle);
-
-        let pokeImg = document.createElement("img");
-        pokeImg.src = pokeObj.img;
-        card.append(pokeImg);
-
-        pokeNums.shift();
-      });
+    let pokeImg = document.createElement("img");
+    pokeImg.src = pokeObj.img;
+    card.append(pokeImg);
   });
+  pokeNums.shift();
 });
